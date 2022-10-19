21665548dbda3f4357fade261faa703c90eb5ebb
amahmood1718 <59544387+amahmood1718@users.noreply.github.com>
2022-10-19 10:38:12 -0400

    Add files via upload

--- antichessbot.py
+++ antichessbot.py
@@ -1,19 +1,47 @@
 import chess
+import random
 
 """Remind me to teach you how to set up a custom board so that you can do your testing"""
 
-
+#The play function is the one that is called to play against the bot
 #note that player here is the bot
-#This function should probably have a while loop which iterates while game is not over
-#The function should also read the moves we play as input so we have a back and forth
 def play(player):
-    return 0
+
+    #first move
+    board = chess.Board()
+    if player == "white":
+        bot_move = random_move(board)
+        board.push_san(bot_move)
+    move = input()
+    board.push_san(move)
+    
+    #game plays until it is over
+    while not board.is_game_over():
+        bot_move = random_move(board)
+        board.push_san(bot_move)
+        move = input()
+        board.push_san(move)
+
+    pass
 
 #this function returns the random move to be played
 def random_move(board):
-    return 0
+    #convert standard to coordinate
+    standard_moves = board.legal_moves  
+    coordinate_moves = []
+    for x in standard_moves:
+        coordinate_moves.append(str(x))
+    
+    #getting set of captures
+    captures = legal_captures(board, coordinate_moves)
 
-#returns the set of legal captures, note that legal_moves is in coordinate algebraic notation
+    if len(captures) == 0:
+        return random.choice(coordinate_moves)
+    else:
+        return random.choice(captures)
+
+#returns the set of legal captures
+#note that legal_moves is in coordinate algebraic notation
 def legal_captures(board, legal_moves):
     captures = []
     for x in legal_moves:
@@ -21,12 +49,3 @@ def legal_captures(board, legal_moves):
         if board.piece_at(z) != None:
             captures.append(x)
     return captures
-
-
-#use this code to convert standard notation to coordinate location
-"""
-standard_moves = board.legal_moves  
-coordinate_moves = []
-for x in standard_moves:
-    coordinate_moves.append(str(x))
-"""
